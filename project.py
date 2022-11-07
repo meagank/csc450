c8414157bb88a33360b21e9351a830ec1c66ae23
Meagan Kropp <meagan.kropp@gmail.com>
2022-11-07 17:47:15 -0600

    Update project.py

--- project.py
+++ project.py
@@ -2,18 +2,31 @@ import csv
 import sys
 
 
-file = sys.argv[1]
-letter = input("Please, provide the source node: ")
 
-with open(file,'r') as csvFile:
-    fileInput = csv.reader(csvFile)
-    fileArray = []
+def fileValues(file):
+    with open(file,'r') as csvFile:
+        fileInput = csv.reader(csvFile)
+        fileArray = []
 
-    for n in fileInput:
-        fileArray.append(n)
+        for n in fileInput:
+            fileArray.append(n)
 
-nodes = []
-for n in range(len(fileArray[0])):
-    nodes.append(fileArray[0][n])
+    return fileArray
 
-print(nodes)
+
+def nodeNames(fileArray):
+# just node names
+    nodes = []
+    for n in range(1,len(fileArray[0])):
+        nodes.append(fileArray[0][n])
+    return(nodes)
+
+def main():
+    file = sys.argv[1]
+    letter = input("Please, provide the source node: ")
+    fileArray = fileValues(file)
+    nodeArray = nodeNames(fileArray)
+
+    print (nodeArray)
+
+main()
