14e5ca02a05d84bef32d8e3fbc582d27e9a4b656
Sydfree <62077218+Sydfree@users.noreply.github.com>
2022-11-12 21:33:23 -0600

    Update project.py

--- project.py
+++ project.py
@@ -1,28 +1,124 @@
 import csv
 import sys
 
+with open(file,'r') as csvFile:
+    fileInput = csv.reader(csvFile)
+    fileArray = []
 
+    for n in fileInput:
+        fileArray.append(n)
 
-def getValues(file):
-    with open(file,'r') as csvFile:
-        fileInput = csv.reader(csvFile)
-        data = []
+nodes = []
+for n in range(len(fileArray[0])):
+    nodes.append(fileArray[0][n])
 
-        for n in fileInput:
-            data.append(n)
+print(nodes)
 
-        data[0].remove("")
-        names = data[0]
+def recur(previous:dict, source:str, current:str):
+    if current == source:
+        return source
+    return recur(previous, source, previous[current] + current)
 
-    return data,names
+def shortestTree(previous:dict, source:str):
+    tree = {}
+    for i in previous:
+        #recur function requries previous, source, and current node
+        tree[i] = recur(previous, source, i)
+    return tree
 
+#implementing Dijkstra's algorithm to calculate shortest path tree/
+#cost of least cost path
+def dijAlg(nodes:list, source:str):
+    #recreating equation from slides
+    #D(v)- current cost of least cost path
+    D = {}
+    #p(v)- previous node in path
+    p = {}
+    #N'- set of nodes where least cost path known
+    N = list(source)
 
-def main():
+    for currentNode in nodes:
+        #if v adj to u
+        if(cost[currentNode][source] != 9999):
+            #then D(v) = c(u,v)
+            D[currentNode] = cost[currentNode][source]
+            p[currentNode] = source
+        else:
+            #else D(v) = inf
+            D[CurrentNode] = 9999
+
+    currentNode = source
+
+    #repeat algorithm
+    while len(N) < len(nodes):
+        #find w not in N'
+        Dw = {k:v for k,v in D.items() if k not in N}
+        w = min(Dw, key = Dw.get)
+        N.append(w)
+
+        exNodes = [i for i in nodes if i not in N]
+        for currentNode in exNodes:
+            #D(v) = minimum of D(v) and D(w) + c(w,v)
+            nextD = min(D[currentNode], (D[w] + cost[w][currentNode]))
+            if(nextD != D[currentNode]):
+                D[currentNode] = nextD
+                p[currentNode] = w
+
+    p = shortestTree(p, source)
+
+    return(D, p)
+
+#creating the Node class
+class Node:
+    allNodes = {}
+
+    def __init__(self, name, nodes):
+        self.name = name
+        self.Dx = self.Cx = {i:cost[name][i] for i in nodes}
+
+        inf = {i:9999 for i in nodes}
+        self.Dv = {i:inf for i in nodes}
+        self.Dv[self.name] = self.Dx
+        self.adjacent = [i for i in self.Dx if i != name and self.Cx[i] != 9999]
+
+        Node.allNodes.update({name:self})
+        self.updateDv()
+
+#implementing Bellman-Ford algorithm to calculate distance vector
+    #def bellFord
+
+    def updateDv(self):
+        for node in self.adjacent:
+            if node in Node.allNodes:
+                currentNode = Node.allNodes[node]
+                self.Dv[currentNode.name] = currentNode.Dx
+                self.bellFord(currentNode.name)
+
+                currentNode.Dv[self.name] = self.Dx
+                currentNode.bellFord(self.name)
+
+    def __str__(self):
+        return ', '.join([str(self.Dx[i]) for i in self.Dx])
+
+#implementing the distance vector function
+def distanceVector(cvsFile:list):
+    return {i:Node(i, csvFile) for i in csvFile}
+
+#main function
+if __name__ == '__main__':
     file = sys.argv[1]
-    letter = input("Please, provide the source node: ")
-    data,names = getValues(file)
+    source = input("Please, provide the source node: ")
+    data = readCSV(file)
+    data[0].remove("")
+    header = data[0]
+    cost = {i[0]:{header[j]:int(i[1:][j]) for j in range(len(i[1:]))} for i in data[1:]}
 
-    print(data)
-    print(names)
+    #calling Dijkstra's Algorithm
+    D, p = dijAlg(data[0], source)
+    tree = ', '.join(p.values())
+    costs = ', '.join([f"{k}:{v}" for k,v in D.items()])
 
-main()
+    #print statements
+    print("Shortest path tree for node {}:\n {}".format(source, tree))
+    print("Costs of least-cost paths for node {}:\n {}".format(source, costs))
+    print("\nDistance vector for node {}:", distance)
